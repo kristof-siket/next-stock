95bf30f986b35b7e53f9fc15c912f528cdd09e80
Kristof Siket <siket@prisma.io>
2026-08-19 14:36:46 +0200

    Revert "Merge pull request #1 from kristof-siket/prisma-cloud/setup"
    This reverts commit 1f0033871b8ce3444667cf68dcf5bc44feaf023d, reversing
    changes made to 7c842b4fc3496d51431767cf290c74f7305c95e2.

--- module.ts
+++
@@ -1,6 +0,0 @@
-import { module } from "@prisma/composer";
-import nextStockService from "./service.js";
-
-export default module("next-stock", ({ provision }) => {
-  provision(nextStockService, { id: "nextstock" });
-});
